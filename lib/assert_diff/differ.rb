2f23ec1f7a982674d746e0a595e0133037ac2abe
Maxime Bedard <maxime.bedard@shopify.com>
2016-04-13 11:42:42 -0400

    Add regex and class matching support

--- lib/assert_diff/differ.rb
+++ lib/assert_diff/differ.rb
@@ -16,24 +16,36 @@ module AssertDiff
       private
 
       def compare(a, b)
-        # Do pattern matching here/class matching/whatev
-        a == b
+        # byebug
+        case b
+        when Regexp
+          a =~ b
+        when Class, Symbol
+          # byebug
+          a.is_a?(b)
+        else
+          a == b
+        end
       end
 
       def diff_hash(a, b, strict: true)
         keys = strict ? a.keys | b.keys : a.keys & b.keys
 
         keys.each_with_object([{}, {}]) do |k, memo|
-          next memo unless match = diff(a[k], b[k])
+          next memo unless match = diff(a[k], b[k], strict: strict)
 
           memo[0][k], memo[1][k] = match
         end
       end
 
-      def diff_array(a, b)
-        a.zip(b).map do |args|
-          diff(*args) || [nil, nil]
+      def diff_array(a, b, strict: true)
+        padright(a, [a.size, b.size].max).zip(b).map do |args|
+          diff(*args, strict: strict) || [nil, nil]
         end.transpose
+      end
+
+      def padright(a, n)
+        a.dup.fill(nil, a.length...n)
       end
     end
   end
--- test/assert_diff_test.rb
+++ test/assert_diff_test.rb
@@ -1,8 +1,30 @@
 require 'test_helper'
 
 class AssertDiffTest < Minitest::Test
+  include AssertDiff
+
   def test_that_it_has_a_version_number
     refute_nil ::AssertDiff::VERSION
   end
 
+  def test_assert_hash_match
+    assert_strict_diff(
+      {"checkout" => { "token" => SecureRandom.hex, "billing_address" => { "zip" => "90210" } } },
+      {"checkout" => { "token" => /\w+/, "billing_address" => { "zip" => "90210" } } },
+    )
+  end
+
+  def test_assert_hash_intersect
+    assert_loose_diff(
+      {"checkout" => { "token" => "123123", "billing_address" => { "zip" => "90210" } } },
+      {"checkout" => { "token" => /\d+/ } },
+    )
+  end
+
+  def test_assert_hash_intersect
+    assert_loose_diff(
+      {"checkout" => { "token" => 123123, "billing_address" => { "zip" => "90210" } } },
+      {"checkout" => { "token" => Integer } },
+    )
+  end
 end
