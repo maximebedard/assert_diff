54d73bea1677d52d5d7bb605ddea141481fddd46
Maxime Bedard <maxime.bedard@shopify.com>
2016-05-21 17:10:54 -0400

    Add rubocop + ruby-version files

--- lib/assert_diff.rb
+++ lib/assert_diff.rb
@@ -2,11 +2,11 @@ require "assert_diff/version"
 require "assert_diff/differ"
 
 module AssertDiff
-  def assert_strict_diff(expected, actual)
+  def assert_diff_equal(expected, actual)
     assert_equal(*Differ.diff(expected, actual))
   end
 
-  def assert_loose_diff(expected, actual)
+  def assert_diff_intersect(expected, actual)
     assert_equal(*Differ.diff(expected, actual, strict: false))
   end
 end
